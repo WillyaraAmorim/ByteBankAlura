5358fe3be8def63fc1cad7b76339a8ab7ec51c13
Willyara Amorim <willyara.amorim@cea.com.br>
2022-02-07 14:05:45 -0300

    Projeto 01 da ByteBank

---
+++ ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //criando um objeto (instancia) (chamando a classe ContaCorrente) new ContaCorrente
+            //A contaDaGabriela está apontando para a classe ContaCorrente
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            //Estou acessando as variáveis da classe.
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.conta = 863452;
+            contaDaGabriela.saldo = 100;
+            Console.WriteLine("Nome do Titular " + contaDaGabriela.titular);
+            Console.WriteLine("Agencia do Titular " + contaDaGabriela.agencia);
+            Console.WriteLine("Conta do Titular " + contaDaGabriela.conta);
+            Console.WriteLine("Saldo do Titular " + contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo += 200;
+            Console.WriteLine("Saldo do Titular " + contaDaGabriela.saldo);
+
+            Console.ReadLine();
+
+        }
+    }
+}
